377194d3cf7845cfa812885046083ab084f08fa8
ADMIN <smmrdyn02@gmail.com>
2026-02-03 02:47:14 +0800

    Unary Test

--- TestDemoProject/src/Practices/UnaryTest.java
+++ TestDemoProject/src/Practices/UnaryTest.java
@@ -4,5 +4,20 @@ public class UnaryTest {
 	public static void main(String[] args) {
 		int result = +1;
 		System.out.println(result);
+		
+		result--;
+		System.out.println(result);
+		
+		result++;
+		System.out.println(result);
+		
+		result = -result;
+		System.out.println(result);
+		
+		boolean success = false;
+		System.out.println(success);
+		
+		System.out.println(!success);
+		
 	}
 }
