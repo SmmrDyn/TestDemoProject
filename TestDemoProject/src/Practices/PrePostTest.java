b5b77177cc3c13ceaf189f9f79bc04a442119a94
ADMIN <smmrdyn02@gmail.com>
2026-02-03 03:53:31 +0800

    PrePost Test

---
+++ TestDemoProject/src/Practices/PrePostTest.java
@@ -0,0 +1,21 @@
+package Practices;
+
+public class PrePostTest {
+	public static void main(String[] args) {
+		
+		int i = 3;
+		
+		i++;
+		System.out.println(i);
+		
+		++i;
+		System.out.println(i);
+		
+		
+		System.out.println(++i);
+		
+		System.out.println(i++);
+
+		System.out.println(i);
+	}
+}
