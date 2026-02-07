4971160471fe6982267b6bcd77f3004523e64406
ADMIN <smmrdyn02@gmail.com>
2026-02-08 03:30:23 +0800

    Random

---
+++ TestDemoProject/src/Practices/RandomNumbers.java
@@ -0,0 +1,18 @@
+package Practices;
+
+import java.util.Random;
+
+public class RandomNumbers {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+//		int x = random.nextInt(6)+1;
+//		double y = random.nextDouble();
+		boolean z = random.nextBoolean();
+		
+		System.out.println(z);
+	}
+
+}
