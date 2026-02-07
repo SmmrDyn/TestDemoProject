3f35c1d1baf8ad6c8bc12ded03a234ad86a0b81c
ADMIN <smmrdyn02@gmail.com>
2026-02-08 01:54:10 +0800

    Expression

---
+++ TestDemoProject/src/Practices/Expressions.java
@@ -0,0 +1,18 @@
+package Practices;
+
+public class Expressions {
+
+	public static void main(String[] args) {
+		// expressions = operands & operators
+		// operands = values, variables, numbers, quantity
+		// operators = + - * / %
+		
+		double friends = 10;
+		
+		friends = (double) friends / 3;
+		
+		System.out.println(friends);
+
+	}
+
+}
