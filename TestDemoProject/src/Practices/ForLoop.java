7a3f6dac5b600ee12a6213137b6bec1e5045e265
ADMIN <smmrdyn02@gmail.com>
2026-02-10 08:04:38 +0800

    For Loop

---
+++ TestDemoProject/src/Practices/ForLoop.java
@@ -0,0 +1,18 @@
+package Practices;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		 // for loop - execute limited amount of times
+		
+		for (int i = 10; i >= 0; i--) {
+		//index declared | condition | increment/decrement
+						
+			System.out.println(i);
+		}
+		
+		System.out.println("Yey!");
+
+	}
+
+}
