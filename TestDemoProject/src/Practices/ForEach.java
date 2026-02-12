aff5ba75b82a308e05b303aed52fac598e86fb0f
ADMIN <smmrdyn02@gmail.com>
2026-02-12 23:01:12 +0800

    For Each

---
+++ TestDemoProject/src/Practices/ForEach.java
@@ -0,0 +1,29 @@
+package Practices;
+
+import java.util.ArrayList;
+
+public class ForEach {
+
+	public static void main(String[] args) {
+		//for each - technique to iterate through the elements in an array/collection
+		//less steps, more readable
+		//less flexible
+		
+//		String[] animals = {
+//				"cat", "dog", "rat", "bird"
+//		};
+		
+		ArrayList<String> animals = new ArrayList<String>();
+		
+		animals.add("cat");
+		animals.add("dog");
+		animals.add("rat");
+		animals.add("bird");
+		
+		for(String i : animals) {
+			System.out.println(i);
+		}
+
+	}
+
+}
