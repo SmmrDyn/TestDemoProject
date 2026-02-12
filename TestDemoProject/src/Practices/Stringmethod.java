3314b2c6276afbec5f1c12497d8111091784a0ce
ADMIN <smmrdyn02@gmail.com>
2026-02-12 21:58:34 +0800

    String Methods

---
+++ TestDemoProject/src/Practices/Stringmethod.java
@@ -0,0 +1,23 @@
+package Practices;
+
+public class Stringmethod {
+
+	public static void main(String[] args) {
+		String name = "Bro";
+		
+//		int result = name.length();
+//		char result = name.charAt(0);
+//		int result = name.indexOf("o");
+//		boolean result = name.equalsIgnoreCase("bro");
+//		boolean result = name.isEmpty();
+//		String result = name.toUpperCase();
+//		String result = name.toLowerCase();
+//		String result = name.trim();
+		String result = name.replace('o', 'a');
+		
+		
+		
+		System.out.println(result);
+	}
+
+}
