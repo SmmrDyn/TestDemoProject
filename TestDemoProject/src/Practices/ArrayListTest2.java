db8cd2d9c3ec1d871ba4346e781c1d0553aa48c4
ADMIN <smmrdyn02@gmail.com>
2026-02-12 22:55:30 +0800

    2D Array List

---
+++ TestDemoProject/src/Practices/ArrayListTest2.java
@@ -0,0 +1,38 @@
+package Practices;
+
+import java.util.*;
+
+public class ArrayListTest2 {
+
+	public static void main(String[] args) {
+		//2D ArrayList - dynamic list of lists
+		//can change the size of these lists during runtime
+		
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
+		
+		ArrayList<String> bakeryList = new ArrayList();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("donuts");
+		
+		ArrayList<String> produceList = new ArrayList();
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
+		
+		ArrayList<String> drinksList = new ArrayList();
+		drinksList.add("soda");
+		drinksList.add("coffee");
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinksList);
+		
+		System.out.println(groceryList.get(2).get(1));
+		
+		
+		
+
+	}
+
+}
