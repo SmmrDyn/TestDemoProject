556267db5ba12ec795fee2e1f8111c4892e28349
ADMIN <smmrdyn02@gmail.com>
2026-02-13 14:56:08 +0800

    Method

---
+++ TestDemoProject/src/Practices/Methods.java
@@ -0,0 +1,36 @@
+package Practices;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		//method - a block of code that is executed whenever it is called upon
+//		
+//		String name = "Bro";
+//		int age = 21;
+//		
+//		hello(name, age);
+//		
+//	}
+//	
+//	static void hello(String title, int age) {
+//		System.out.println("Hello " + title);
+//		System.out.println("You are " + age);
+//	}
+	
+	int x = 3;
+	int y = 4;
+	
+//	int z = add(x,y);
+	
+	System.out.println(add(x,y));
+	
+	}
+	
+	static int add(int x, int y) {
+		
+//		int z = x + y;
+		
+		return x + y;
+		
+	}
+}
