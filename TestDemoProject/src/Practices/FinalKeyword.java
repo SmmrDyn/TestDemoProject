971bdb17eea2b6d5f58b482edf99cd4481686d98
ADMIN <smmrdyn02@gmail.com>
2026-02-13 15:32:54 +0800

    Final Keyword

---
+++ TestDemoProject/src/Practices/FinalKeyword.java
@@ -0,0 +1,17 @@
+package Practices;
+
+public class FinalKeyword {
+
+	public static void main(String[] args) {
+		
+		//anything declared as final cannot be changed or updated later in the program
+		
+		final double pi =  3.14159;
+		
+//		pi = 4;
+		
+		System.out.println(pi);
+
+	}
+
+}
