516ac8ee54277b6b660e89ec255a96b9aa9c7a45
ADMIN <smmrdyn02@gmail.com>
2026-02-13 15:45:48 +0800

    Object Oriented Programming

---
+++ TestDemoProject/src/Practices/OOP.java
@@ -0,0 +1,22 @@
+package Practices;
+
+public class OOP {
+
+	public static void main(String[] args) {
+		// object - an instance of a class that may contain attributes and methods
+		// example: phone, desk, computer, coffee cup
+	 
+		OOPCar myCar1 = new OOPCar();
+		OOPCar myCar2 = new OOPCar();
+		
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+		System.out.println( );
+		System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+		
+//		myCar.drive();
+//		myCar.brake();
+		
+	}
+}
---
+++ TestDemoProject/src/Practices/OOPCar.java
@@ -0,0 +1,18 @@
+package Practices;
+
+public class OOPCar {
+	String make = "Chevrolet";
+	String model = "Corvette";
+	int year = 2020;
+	String color = "Blue";
+	double price = 50000.00;
+	
+	void drive() {
+		System.out.println("You drive the car");
+	}
+	
+	void brake() {
+		System.out.println("You step on the break");
+	}
+
+}
