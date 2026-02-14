de541141b9b9b7da0b1233202ac8b800c2d914cc
ADMIN <smmrdyn02@gmail.com>
2026-02-14 15:46:34 +0800

    Abstract Keyword

---
+++ TestDemoProject/src/AbstractKeyword/Car.java
@@ -0,0 +1,11 @@
+package AbstractKeyword;
+
+//subclass or child class of vehicle class
+public class Car extends Vehicle{
+	
+	@Override
+	void go() {
+		System.out.println("The driver is driving the car!");
+	}
+
+}
