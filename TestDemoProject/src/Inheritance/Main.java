a89bbf759c4f723c8ed8fbfa75c395f81fdb8491
ADMIN <smmrdyn02@gmail.com>
2026-02-14 15:07:33 +0800

    Inheritance

---
+++ TestDemoProject/src/Inheritance/Main.java
@@ -0,0 +1,24 @@
+package Inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Inheritance -  the process where one class acquires the attributes and methods of another
+		
+		Car car = new Car();
+
+//		car.stop();
+		
+		Bicycle bike = new Bicycle();
+		
+//		bike.stop();
+		
+//		System.out.println(car.speed);
+//		System.out.println(bike.speed);
+		
+		System.out.println(bike.paddles);
+		System.out.println(car.doors);
+
+	}
+
+}
---
+++ TestDemoProject/src/Inheritance/Vehicle.java
@@ -0,0 +1,15 @@
+package Inheritance;
+
+// it's like an ancestor giving inheritance to its decendants
+public class Vehicle {
+	double speed;
+	
+	void go() {
+		System.out.println("This vehicle is moving.");
+	}
+	
+	void stop() {
+		System.out.println("This vehicle stopped.");
+	}
+
+}
