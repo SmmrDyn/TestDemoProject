0df20db5ec1240eefa834a33d57ddc35d7a8927f
ADMIN <smmrdyn02@gmail.com>
2026-02-20 21:14:16 +0800

    GUI with CheckBox

---
+++ TestDemoProject/src/GUICheckbox/Main.java
@@ -0,0 +1,12 @@
+package GUICheckbox;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// JCheckBox = a GUI component that can be selected of deselected
+		
+		new MyFrame();
+
+	}
+
+}
---
+++ TestDemoProject/src/GUICheckbox/MyFrame.java
@@ -0,0 +1,44 @@
+package GUICheckbox;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame implements ActionListener{
+	
+	JButton button;
+	JCheckBox checkBox;
+	
+	MyFrame() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+		
+		button = new JButton();
+		button.setText("Submit");
+		button.addActionListener(this);
+		
+		checkBox = new JCheckBox();
+		checkBox.setText("I'm useless?");
+		checkBox.setFocusable(false);
+		checkBox.setFont(new Font("Poppins",Font.BOLD,20));
+				
+		this.add(button);
+		this.add(checkBox);
+		this.pack();
+		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==button) {
+			System.out.println(checkBox.isSelected());
+		}
+		
+	}
+
+}
